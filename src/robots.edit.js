dd2656e3252fe1b8721d4d08eace166e0122d1cb
Nat Pryce <sw@natpryce.com>
2014-01-13 09:16:38 +0000

    moved undo stack into edit module

--- src/robots.edit.js
+++ src/robots.edit.js
@@ -50,8 +50,40 @@ define(["modash", "robots.cards2"], function(_, cards) {
 		return new EditPoint(sequence, sequence.length, parent_edit_point, sequence_name);
 	}
 	
+
+	
+	function UndoStack(prevs, current, nexts) {
+		this._prevs = prevs;
+		this._current = current;
+		this._nexts = nexts;
+	}
+	UndoStack.prototype.current = function() {
+		return this._current;
+	};
+	UndoStack.prototype.push = function(new_state) {
+		return new UndoStack([this._prevs, this._current], new_state, null);
+	};
+	UndoStack.prototype.canUndo = function() {
+		return this._prevs != null;
+	};
+	UndoStack.prototype.undo = function() {
+		return new UndoStack(this._prevs[0], this._prevs[1], [this._current, this._nexts]);
+	};
+	UndoStack.prototype.canRedo = function() {
+		return this._nexts != null;
+	};
+	UndoStack.prototype.redo = function() {
+		return new UndoStack([this._prevs, this._current], this._nexts[0], this._nexts[1]);
+	};
+	
+	function undoStartingWith(initial_state) {
+		return new UndoStack(null, initial_state, null);
+	};
+	
 	return {
 		editorsFor: editorsFor,
-		appenderFor: appenderFor
+		appenderFor: appenderFor,
+		undoStartingWith: undoStartingWith
 	};
+
 });
--- test/robots.edit-spec.js
+++ test/robots.edit-spec.js
@@ -90,4 +90,53 @@ define(["lodash", "chai", "robots.cards2", "robots.edit"], function(_, chai, car
 			});
 		});
 	});
+
+
+	describe("Undo Stack", function() {
+		var initial = edit.undoStartingWith(1);
+		
+		it("must be created with initial state", function() {
+			assert.equal(initial.current(), 1, "initial state");
+			assert(!initial.canUndo(), "cannot undo");
+			assert(!initial.canRedo(), "cannot redo");
+		});
+		
+		it("can undo back to previous state after pushing a new state", function() {
+			var u2 = initial.push(2);
+			
+			assert.equal(u2.current(), 2, "new state");
+			assert(u2.canUndo(), "can undo");
+			assert(!u2.canRedo(), "cannot redo");
+			
+			var u1 = u2.undo();
+			assert.equal(u1.current(), 1, "initial state");
+			assert(!u1.canUndo(), "cannot undo");
+			assert(u1.canRedo(), "can redo");
+		});
+		
+		it("can redo to a subsequent state", function() {
+			var u1 = initial.push(2).push(3).undo().undo();
+			
+			assert.equal(u1.current(), 1, "at initial state");
+			assert(u1.canRedo(), "can redo");
+			
+			var u2 = u1.redo();
+			assert(u2.current() === 2, "2nd state");
+			assert(u2.canRedo(), "can redo");
+
+			var u3 = u2.redo();
+			assert(u3.current() === 3, "final state");
+			assert(!u3.canRedo(), "cannot redo");
+		});
+		
+		it("discards redoable states when state is pushed after undoing", function() {
+			var u = initial.push(2).push(3).push(4).undo().undo();
+			assert(u.canRedo(), "can redo");
+			assert.equal(u.current(), 2, "current state");
+			
+			var t = u.push(99);
+			assert.equal(t.current(), 99, "new current state");
+			assert(!t.canRedo());
+		});
+    });
 });

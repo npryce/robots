5360cadd204fbf8c742cc464c60159c9f9336df6
Nat Pryce <sw@natpryce.com>
2014-01-13 08:55:51 +0000

    undo now implemented as a linked-list zipper

--- src/robots.undo.js
+++ src/robots.undo.js
@@ -1,41 +1,32 @@
 define(["d3", "lodash", "js-tree-cursor"], function(d3, _, treecursor) {
 	"use strict";
 	
-	function clearRight(cursor) {
-		return new TreeCursor(
-			cursor.parent,
-			cursor.node,
-			cursor.prevs,
-			[],
-			cursor.openF,
-			cursor.closeF,
-			cursor.atomicF);
-	}
-	
-	function UndoStack(cursor) {
-		this._cursor = cursor;
+	function UndoStack(prevs, current, nexts) {
+		this._prevs = prevs;
+		this._current = current;
+		this._nexts = nexts;
 	}
 	UndoStack.prototype.current = function() {
-		return this._cursor.node;
+		return this._current;
 	};
 	UndoStack.prototype.push = function(new_state) {
-		return new UndoStack(clearRight(this._cursor).insertRight(new_state));
+		return new UndoStack([this._prevs, this._current], new_state, null);
 	};
 	UndoStack.prototype.canUndo = function() {
-		return this._cursor.canLeft();
+		return this._prevs != null;
 	};
 	UndoStack.prototype.undo = function() {
-		return new UndoStack(this._cursor.left());
+		return new UndoStack(this._prevs[0], this._prevs[1], [this._current, this._nexts]);
 	};
 	UndoStack.prototype.canRedo = function() {
-		return this._cursor.canRight();
+		return this._nexts != null;
 	};
 	UndoStack.prototype.redo = function() {
-		return new UndoStack(this._cursor.right());
+		return new UndoStack([this._prevs, this._current], this._nexts[0], this._nexts[1]);
 	};
 	
 	UndoStack.startingWith = function(initial_state) {
-		return new UndoStack(treecursor.arrayToCursor([initial_state]));
+		return new UndoStack(null, initial_state, null);
 	};
 	
 	return UndoStack;

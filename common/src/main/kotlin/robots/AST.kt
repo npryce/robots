6773bb79ebded7fcc0ad5167c7d8c03d585332b1
Nat Pryce <sw@natpryce.com>
2017-11-17 23:16:29 +0530

    Move platform independent code (AST, reduction, etc.) into the common module

---
+++ common/src/main/kotlin/robots/AST.kt
@@ -0,0 +1,12 @@
+package robots
+
+sealed class AST
+data class Action(val name: String) : robots.AST()
+data class Repeat(val times: Int, val repeated: PList<robots.AST>) : robots.AST() {
+    constructor(times: Int, vararg steps: robots.AST) : this(times, pListOf(*steps))
+}
+data class Seq(val steps: PList<robots.AST>) : robots.AST() {
+    constructor(vararg steps: robots.AST) : this(pListOf(*steps))
+}
+
+val nop = robots.Seq(Empty)

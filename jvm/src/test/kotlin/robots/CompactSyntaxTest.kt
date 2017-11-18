3bda78aaf652e056f81d3494caa723e236882298
Nat Pryce <sw@natpryce.com>
2017-11-18 19:13:09 +0530

    Combine parsing and formatting tests for compact syntax

---
+++ jvm/src/test/kotlin/robots/CompactSyntaxTest.kt
@@ -0,0 +1,45 @@
+package robots
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+
+
+@RunWith(Parameterized::class)
+class CompactSyntaxTest(val what: String, val syntax: String, val ast: Seq) {
+    companion object {
+        private val a = Action("a")
+        private val b = Action("b")
+        private val c = Action("c")
+        private val d = Action("d")
+        private val e = Action("e")
+        
+        private val poo = "\uD83D\uDCA9"
+        
+        private val examples: Map<String, Pair<String, Seq>> = mapOf(
+            "empty program" to Pair("", nop),
+            "sequence of single action" to Pair("a", Seq(a)),
+            "sequence of actions" to Pair("a, b, c", Seq(a, b, c)),
+            "nested program" to Pair("a, [b, c], 4•[d, e]", Seq(a, Seq(b, c), Repeat(4, d, e))),
+            "funky identifiers" to Pair("3•[⬆], $poo", Seq(Repeat(3, Action("⬆")), Action(poo)))
+        )
+        
+        @JvmStatic
+        @Parameters(name = "{0}: {1}")
+        fun params() =
+            examples.map { (name, value) -> arrayOf(name, value.first, value.second) }
+    }
+    
+    @Test
+    fun `parses`() {
+        assertThat(what, syntax.toSeq().ok(), equalTo(ast))
+    }
+    
+    @Test
+    fun `formats`() {
+        assertThat(what, ast.toCompactString(), equalTo(syntax))
+    }
+}
--- jvm/src/test/kotlin/robots/example_actions.kt
+++
@@ -1,8 +0,0 @@
-package robots
-
-internal val a = Action("a")
-internal val b = Action("b")
-internal val c = Action("c")
-internal val d = Action("d")
-internal val e = Action("e")
-

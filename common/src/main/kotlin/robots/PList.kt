d6ff0067071900385fde44f1ed93d5372b544c78
Nat Pryce <sw@natpryce.com>
2017-11-18 14:57:33 +0000

    zipper for plists

--- common/src/main/kotlin/robots/PList.kt
+++ common/src/main/kotlin/robots/PList.kt
@@ -1,21 +1,51 @@
 package robots
 
 
-sealed class PList<out T> {
+sealed class PList<out T> : Iterable<T> {
     abstract val head: T?
     abstract val tail: PList<T>
-    abstract fun isEmpty(): Boolean
+    
+    override fun iterator(): Iterator<T> {
+        return PListIterator(this)
+    }
 }
+
 object Empty : PList<Nothing>() {
     override val head get() = null
     override val tail get() = this
-    override fun isEmpty() = true
     override fun toString() = "Empty"
 }
-data class Cons<out T>(override val head: T, override val tail: PList<T>) : PList<T>() {
-    override fun isEmpty() = false
-}
+
+data class Cons<out T>(override val head: T, override val tail: PList<T>) : PList<T>()
+
+fun PList<*>.isEmpty() = this == Empty
+fun PList<*>.isNotEmpty() = !isEmpty()
 
 fun emptyPList() = Empty
 fun <T> pListOf(element: T) = Cons(element, Empty)
 fun <T> pListOf(vararg elements: T) = elements.foldRight(Empty, ::Cons)
+
+
+class PListIterator<T>(private var current: PList<T>) : Iterator<T> {
+    override fun hasNext() =
+        current.isNotEmpty()
+    
+    override fun next() =
+        (current.head ?: throw NoSuchElementException())
+            .also { current = current.tail }
+}
+
+
+class PListZipper<T>(private val back: PList<T>, private val forth: PList<T>) {
+    val current: T? get() = forth.head
+    
+    fun next(): PListZipper<T>? = forth.head?.let { PListZipper(Cons(it, back), forth.tail) }
+    fun prev(): PListZipper<T>? = back.head?.let { PListZipper(back.tail, Cons(it, forth)) }
+    fun remove(): PListZipper<T> = PListZipper(back, forth.tail)
+    fun insert(t: T): PListZipper<T> = PListZipper(back, Cons(t, forth))
+    fun replaceWith(t: T): PListZipper<T> = remove().insert(t)
+    
+    fun toPList(): PList<T> = back.fold(forth, { xs, x -> Cons(x, xs) })
+}
+
+fun <T> PList<T>.zipper() = PListZipper(Empty, this)
---
+++ common/src/test/kotlin/robots/PListZipperTest.kt
@@ -0,0 +1,22 @@
+package robots
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PListZipperTest {
+    @Test
+    fun `traversing`() {
+        val xs = pListOf(1,2,3,4,5,6)
+        val x = xs.zipper().next()?.next()?.current
+        
+        assertEquals(actual = x, expected = 3)
+    }
+    
+    @Test
+    fun `modifying`() {
+        val xs = pListOf(1,2,3,4,5,6)
+        val munged = xs.zipper().next()?.next()?.remove()?.replaceWith(99)?.toPList()
+        
+        assertEquals(actual = munged, expected = pListOf(1, 2, 99, 5, 6))
+    }
+}

fe668be3b40f8ab70c86c1440d4c387ae5763284
Nat Pryce <sw@natpryce.com>
2017-11-27 23:42:31 +0000

    Make DropTarget a class component

--- webui/src/main/kotlin/dnd/DropTarget.kt
+++ webui/src/main/kotlin/dnd/DropTarget.kt
@@ -3,43 +3,57 @@ package dnd
 import org.w3c.dom.Element
 import org.w3c.dom.events.Event
 import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
 import react.dom.div
 
 
-fun RBuilder.dropTarget(
-    canAccept: (Any)->Boolean,
-    accept: (Any)->Unit,
-    contents: RBuilder.()->Unit)
-{
-    fun dragIn(ev: Event) {
+class DropTarget(props: DropTarget.Props): RComponent<DropTarget.Props, RState>(props) {
+    interface Props: RProps {
+        var canAccept: (Any)->Boolean
+        var accept: (Any)->Unit
+    }
+    
+    private fun dragIn(ev: Event) {
         val detail: DragInDetail = ev.detail() ?: return
-        detail.acceptable = canAccept(detail.data)
+        detail.acceptable = props.canAccept(detail.data)
     }
     
-    fun dragOut(it: Event) {
+    private fun dragOut(it: Event) {
     }
     
-    fun drop(ev: Event) {
+    private fun drop(ev: Event) {
         val detail: DropDetail = ev.detail() ?: return
         
-        if (canAccept(detail.data)) {
-            accept(detail.data)
+        if (props.canAccept(detail.data)) {
+            props.accept(detail.data)
             detail.accepted = true
         } else {
             detail.accepted = false
         }
     }
     
-    div("drop-target") {
-        ref { elt: Element? ->
-            if (elt != null) {
-                elt.addEventListener(DND_DRAG_IN, ::dragIn)
-                elt.addEventListener(DND_DRAG_OUT, ::dragOut)
-                elt.addEventListener(DND_DROP, ::drop)
+    override fun RBuilder.render() {
+        div("drop-target") {
+            ref { elt: Element? ->
+                if (elt != null) {
+                    elt.addEventListener(DND_DRAG_IN, ::dragIn)
+                    elt.addEventListener(DND_DRAG_OUT, ::dragOut)
+                    elt.addEventListener(DND_DROP, ::drop)
+                }
             }
-        }
         
-        contents()
+            children()
+        }
     }
 }
+
+fun RBuilder.dropTarget(canAccept: (Any)->Boolean, accept: (Any)->Unit, children: RBuilder.()->Unit) =
+    child(DropTarget::class) {
+        attrs.canAccept = canAccept
+        attrs.accept = accept
+        
+        children()
+    }
 

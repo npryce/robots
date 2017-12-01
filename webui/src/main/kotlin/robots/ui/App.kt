8cea83f601093326788eee92daf247d6f104ab64
Nat Pryce <sw@natpryce.com>
2017-12-01 19:30:29 +0000

    Undo/Redo control buttons

--- webui/src/main/kotlin/robots/ui/App.kt
+++ webui/src/main/kotlin/robots/ui/App.kt
@@ -1,15 +1,23 @@
 package robots.ui
 
+import kotlinx.html.DIV
+import kotlinx.html.js.onClickFunction
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
+import react.dom.RDOMBuilder
+import react.dom.button
 import react.dom.div
 import react.dom.span
 import robots.Seq
 import robots.UndoRedoStack
+import robots.canRedo
+import robots.canUndo
 import robots.cost
 import robots.havingDone
+import robots.redo
+import robots.undo
 
 
 external interface AppProps : RProps {
@@ -26,13 +34,17 @@ class App(props: AppProps) : RComponent<AppProps, AppState>(props) {
     }
     
     override fun RBuilder.render() {
-        header(state.undo.current)
+        header(state.undo, update = ::updateUndoRedoStack)
         programEditor(state.undo.current, onEdit = ::pushUndoRedoState)
         controlPanel()
     }
     
     private fun pushUndoRedoState(newProgram: Seq) {
-        setState({ it.apply { undo = undo.havingDone(newProgram) } })
+        updateUndoRedoStack(state.undo.havingDone(newProgram))
+    }
+    
+    private fun updateUndoRedoStack(newState: UndoRedoStack<Seq>) {
+        setState({ it.apply { undo = newState } })
     }
 }
 
@@ -41,10 +53,29 @@ fun RBuilder.app(initialProgram: Seq = Seq()) = child(App::class) {
     attrs.program = initialProgram
 }
 
-fun RBuilder.header(program: Seq) {
+fun RBuilder.header(undoStack: UndoRedoStack<Seq>, update: (UndoRedoStack<Seq>) -> Unit) {
     div("header") {
-        +"Cost: "
-        span("cost") { +"£${program.cost()}" }
+        span("score") {
+            +"Cost: "
+            span("cost") { +"£${undoStack.current.cost()}" }
+        }
+        
+        undoRedoButtons(undoStack, update)
+    }
+}
+
+private fun RDOMBuilder<DIV>.undoRedoButtons(undoStack: UndoRedoStack<Seq>, update: (UndoRedoStack<Seq>) -> Unit) {
+    span("edit-controls") {
+        button {
+            attrs.onClickFunction = { update(undoStack.undo()) }
+            attrs.disabled = !undoStack.canUndo()
+            +"Undo"
+        }
+        button {
+            attrs.onClickFunction = { update(undoStack.redo()) }
+            attrs.disabled = !undoStack.canRedo()
+            +"Redo"
+        }
     }
 }
 

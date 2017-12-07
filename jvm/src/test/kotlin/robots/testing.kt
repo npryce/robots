ee86be63bf0d1c254caf331234eb3dff13c032be
Nat Pryce <sw@natpryce.com>
2017-12-07 15:36:10 +0000

    Tidy

--- jvm/src/test/kotlin/robots/testing.kt
+++ jvm/src/test/kotlin/robots/testing.kt
@@ -3,6 +3,5 @@ package robots
 import com.natpryce.Result
 import com.natpryce.onError
 
-internal inline fun <reified T, E> Result<T, E>.ok(): T {
-    return onError { throw AssertionError("expected Ok<${T::class.simpleName}>, was ${this}")}
-}
+internal inline fun <reified T, E> Result<T, E>.ok() =
+    onError { throw AssertionError("expected Ok<${T::class.simpleName}>, was ${this}")}
